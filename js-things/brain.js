c280d30015a448a26428868b8e0206c411085f72
Solomzi Jikani <solomzi.jikani@nologostudios.com>
2018-11-04 23:09:00 +0200

    refactor

--- js-things/brain.js
+++
@@ -1,9 +0,0 @@
-window.commands = [
-    { realname: "Solomzi Jikani"},
-    { dob: "I was born in September 29, 1994"},
-    { education: "Port St. Johns High School (3 years) | UniCollege (6 months) | Durban University of Technology (4 years)"},
-    { contacts: "I was born in September 29, 1994"},
-    { devcontacts: "I was born in September 29, 1994"},
-    { relstatus: "I was born in September 29, 1994"},
-    { skills: "I was born in September 29, 1994"}
-  ];
