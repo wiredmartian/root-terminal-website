fe57baed4a120ea74af5713c5fa52b98dbef171e
wiredmartian <meggatym@yahoo.com>
2018-11-09 00:33:01 +0200

    clear typewriter. defien commands in external file

---
+++ js/data.js
@@ -0,0 +1,8 @@
+window.commands = [
+    { name : "Solomzi Jikani" },
+    { location: "Kloof, KwaZulu-Natal in South Africa"},
+    { languages: "Xhosa (native), and English"},
+    { email: "solomzi.jikani@gmail.com"},
+    { phone: "+27 71 786 2455"},
+    { employment: "I'm currently a front-end developer at Nologo Studios"},
+    { profession: "Software Developer"}];
