7b348d8ca69ca16340e7a5c3434b2f1d197e6122
Solomzi Jikani <solomzi.jikani@nologostudios.com>
2018-11-09 16:01:24 +0200

    refactor scss... what?

--- js/data.js
+++ js/data.js
@@ -5,4 +5,5 @@ window.commands = [
     { email: "solomzi.jikani@gmail.com"},
     { phone: "+27 71 786 2455"},
     { employment: "I'm currently a front-end developer at Nologo Studios"},
-    { profession: "Software Developer"}];
+    { profession: "Software Developer"},
+    { help: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet magna a lorem dapibus volutpat eu et mauris. Donec sit amet pulvinar leo. Quisque nec laoreet nunc. Vestibulum semper vitae ante a facilisis. Sed vel maximus leo. Aenean tincidunt aliquam volutpat. Phasellus suscipit risus vitae eros aliquet, sit amet pharetra eros blandit. Sed suscipit elit tortor, ut rutrum mi suscipit quis. Fusce volutpat orci mi, sit amet ultricies diam ultricies pulvinar. "}];
