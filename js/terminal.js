a3645b986586b51315a54576ae2959b7998ecec6
Solomzi Jikani <solomzi.jikani@nologostudios.com>
2018-12-03 16:21:54 +0200

    cleaned up window terminal format

--- js/terminal.js
+++ js/terminal.js
@@ -10,7 +10,6 @@ function Terminal(element, options) {
             if (res) {
                 _defaults();
                 _getOptions(_self.options);
-                dragElement("#terminal-window");
             }
         });
     }
@@ -181,72 +180,26 @@ function Terminal(element, options) {
         return result;
     }
 
-    /** make the terminal draggable */
-
-    function dragElement(el) {
-        el = document.querySelector(el);
-        let title_block = document.querySelector("#window-title-bar");
-
-        let position1 = 0, position2 = 0, position3 = 0, position4 = 0;
-        if (title_block) {
-            title_block.onmousedown = dragMouseDown;
-        } else {
-            el.onmousedown = dragMouseDown;
-        }
-
-        function dragMouseDown(e) {
-            e = e || window.event;
-            e.preventDefault();
-            position3 = e.clientX;
-            position4 = e.clientY;
-            document.onmouseup = closeDragElement;
-            document.onmousemove = elementDrag;
-        }
-
-        function elementDrag(e) {
-            e = e || window.event;
-            e.preventDefault();
-            position1 = position3 - e.clientX;
-            position2 = position4 - e.clientY;
-            position3 = e.clientX;
-            position4 = e.clientY;
-            el.style.top = (el.offsetTop - position2) + "px";
-            el.style.left = (el.offsetLeft - position1) + "px";
-        }
-
-        function closeDragElement() {
-            document.onmouseup = null;
-            document.onmousemove = null;
-        }
-    }
-
-    function _loadTerminalHTML(cb) {
+    function _loadTerminalHTML(callabck) {
         // language=HTML
-        let _template = "<div class=\"window\">" +
-            "    <div id=\"terminal-window\" class=\"terminal\">" +
-            "        <div id=\"window-title-bar\">root@wiredmartian:~</div>" +
-            "        <div class=\"typewriter-container\"><span id=\"typewriter\"></span></div>" +
-            "        <div class=\"terminal-content\">" +
-            "            <div class=\"line\">" +
-            "                <span class=\"prefix\">guest@user:~# </span>" +
-            "                <span></span>" +
-            "                <small id=\"commandInput\" class=\"caret\" contenteditable=\"true\" spellcheck=\"false\">.</small>" +
-            "            </div>" +
-            "        </div>" +
-            "        <div class=\"clearfix\"></div>" +
-            "    </div>" +
+        let _template = "<div class=\"window-title-bar\">root@wiredmartian:~</div>\n" +
+            "<div id=\"window\" class=\"terminal\">\n" +
+            "    <div class=\"typewriter-container\"><span id=\"typewriter\"></span></div>\n" +
+            "    <div class=\"line\">\n" +
+            "        <span class=\"prefix\">guest@user:~# </span>\n" +
+            "        <span></span>\n" +
+            "        <small id=\"commandInput\" class=\"caret\" contenteditable=\"true\" spellcheck=\"false\">.</small>\n" +
+            "    </div>\n" +
             "</div>";
 
         let _container = document.querySelector("#terminal-container");
         if (_container && _checkOutputIsHTML(_template)) {
             _container.innerHTML = _template;
             initializeTyping();
-            cb(true);
+            callabck(true);
         } else {
-            cb()
+            callabck();
         }
-
-
     }
     function initializeTyping() {
         let intro = "<span style=\"color:#21f838\"><small>Installing wm-terminal...</small></span>^3000<br>" +
