45be1c6788260d9fa1a364c56ee1e18eeea70a7f
wiredmartian <meggatym@yahoo.com>
2020-09-02 22:36:22 +0200

    updates - site might be complete

--- src/js/data.js
+++ src/js/data.js
@@ -1,23 +1,19 @@
 export let terminalcommands = [
-    { name: "My real name is Solomzi Jikani" },
-    { location: "KwaZulu-Natal in South Africa"},
-    { email: "solomzi.jikani@gmail.com"},
     { contact: "<br><b>Email:</b> solomzi.jikani@gmail.com <br/>" +
-               "<b>Mobile:</b> 071 786 2455 <br/>" +
-               "<b>Github:</b> <a href='https://github.com/wiredmartian'>-- click here to go to my github --</a>"},
+               "<b>Mobile:</b> 071 786 2455 <br/>"},
     { experience: 
         
         `<br><br> <b>Izingcweti Digital Advisory (Feb 2017 - Dec 2017):</b> <span class='prefix-root'>Full-stack developer (part-time internship)</span>
         <br> <b>Nologo Studios (March 2018 - April 2019):</b> <span class='prefix-root'>Front-end developer full-time</span>
         <br> <b>Mytelnet (August 2019 - Present):</b> <span class='prefix-root'>Full-stack developer full-time</b>`
     },
-    { stack: "<br/><br/><b> --- Programming languages --- </b><br/>" +
+    { languages: "<br/><br/><b> --- Programming languages --- </b><br/>" +
             "<br/> Nodejs (Fastify, Express & Restify)" +
             "<br/> C# (MVC5, Web API2 & .NET Core 3.x)" +
             "<br/> GoLang" +
             "<br/> TypeScript/JavaScript" +
             "<br/> HTML/CSS" +
-            "<br/><br/><b>  --- Front-end Frameworks --- </b>" +
+            "<br/><br/><b>  --- Frontend Frameworks --- </b>" +
             "<br/> Vuejs 2+" +
             "<br/> Angular 2+" +
             "<br/><br/><b> --- Mobile --- </b>" +
@@ -31,5 +27,14 @@ export let terminalcommands = [
     },
     {
 // language=HTML
-        bio:"<br><br>My name is Solomzi Jikani, a <b>full-stack software developer</b> based in Durban, South Africa..."}
+        about:"<br><br>My name is Solomzi Jikani, a <b>qualified full-stack software developer</b> based in Durban, South Africa. " +
+        "I am also a <b>YouTuber</b>, a minimalist, and I'm passionate about `all-things-high-performance`. I am originally from Port St Johns " +
+        ", Eastern Cape. I ended up in KZN as it is where I did my tertiary education. I understand 4 languages, and can speak 2 fluently. " +
+        " <b>I can write in 4 programming languages</b>, and I hope one day to write in C. I have an <b>imposter syndrome</b>, it is my greatest strength " +
+        ", and my greatest weakness. My weekends are mostly hikes, or hanging with friends for some football games."},
+
+    {
+        sourecode: "The source code for this website is available on my <a href='https://github.com/wiredmartian/root-terminal' target='_blank'>github</a> :)"
+    }
+
 ];
